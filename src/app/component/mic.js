1d44544c45bca89b1080e52d8e112e63ee0bfdb0
Rahul Kumar <rahul.kumar@softage.net>
2023-10-11 12:33:39 +0530

    rahul kumar

---
+++ src/app/component/mic.js
@@ -0,0 +1,39 @@
+'use client'
+import 'regenerator-runtime/runtime'
+import React , { useEffect } from 'react';
+import useSpeechToText from 'react-hook-speech-to-text';
+ const Mic = () => {
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: false,
+    crossBrowser: true,
+    googleApiKey: 'AIzaSyBYFQzw3V4bDy8nV1IT0euVcEtJ6MzUX4U',
+    useLegacyResults: false
+  });
+
+
+  if (error){"Web Speech API is not available in this browser"};
+
+  return (
+    <div>
+      <h1>Recording: {isRecording.toString()}</h1>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+      <ul>
+        {results.map((result) => (
+          <li key={result.timestamp}>{result.transcript}</li>
+        ))}
+        {interimResult && <li>{interimResult}</li>}
+      </ul>
+    </div>
+  );
+}
+export default Mic;
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,9 @@
 'use client'
-import 'regenerator-runtime/runtime'
-import React , { useEffect } from 'react';
-import useSpeechToText from 'react-hook-speech-to-text';
-const Speech = () => {
-  useEffect(() => {
-    // Access the navigator object here
-    const userAgent = navigator.userAgent;
-    console.log(userAgent);
-  }, []);
-  const {
-    error,
-    interimResult,
-    isRecording,
-    results,
-    startSpeechToText,
-    stopSpeechToText,
-  } = useSpeechToText({
-    continuous: true,
-    crossBrowser: true,
-    googleApiKey: 'AIzaSyBYFQzw3V4bDy8nV1IT0euVcEtJ6MzUX4U',
-    useLegacyResults: false
-  });
-
-
-  if (error){"Web Speech API is not available in this browser"};
-
-  return (
-    <div>
-      <h1>Recording: {isRecording.toString()}</h1>
-      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
-        {isRecording ? 'Stop Recording' : 'Start Recording'}
-      </button>
-      <ul>
-        {results.map((result) => (
-          <li key={result.timestamp}>{result.transcript}</li>
-        ))}
-        {interimResult && <li>{interimResult}</li>}
-      </ul>
-    </div>
+import Mic from "./component/mic";
+export default function Home() {
+ return (
+<div>hello
+<Mic/>
+</div>
   );
 }
-export default Speech;
