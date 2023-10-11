9370ef69446960f0ad0fe5fde293281aed86da22
Rahul Kumar <rahul.kumar@softage.net>
2023-10-11 17:08:44 +0530

    change continous value ture to false

--- src/app/page.js
+++ src/app/page.js
@@ -3,25 +3,33 @@ import { useEffect, useState } from 'react';
 export default function Home() {
   const [isListening, setIsListening] = useState(false);
   const [transcript, setTranscript] = useState('');
-  const recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+  const [recognition, setRecognition] = useState();
 
   useEffect(() => {
-    recognition.onresult = function (event) {
-      const result = event.results[0][0].transcript;
-      console.log(result)
-      setTranscript(result);
-      if (isListening) {
-        recognition.start();
-      }
-    };
-
-    recognition.onend = function () {
-      if (isListening) {
-        recognition.start();
-      }
-    };
-  }, [isListening]);
-
+    if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+      // SpeechRecognition is supported
+      const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+      setRecognition(recognition)
+      recognition.onresult = function (event) {
+        const result = event.results[0][0].transcript;
+        setTranscript(result);
+        if (isListening) {
+          recognition.start();
+        }
+      };
+      recognition.onend = function () {
+        if (isListening) {
+          recognition.start();
+        }
+      };
+      recognition.onerror = (event) => {
+        console.log(`Speech recognition error detected: ${event.error}`);
+        console.log(`Additional information: ${event.message}`);
+      };
+    } else {
+      console.log('Speech recognition is not supported in this browser.');
+    }
+  }, []);
   const toggleListening = () => {
     if (!isListening) {
       setIsListening(true);
@@ -31,7 +39,6 @@ export default function Home() {
       recognition.stop();
     }
   };
-
 
   return (
     <div>
