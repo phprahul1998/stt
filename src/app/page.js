8318672e3b9c3f5d548ca4494f98e48741fd5765
Rahul Kumar <rahul.kumar@softage.net>
2023-10-11 11:26:37 +0530

    rahul kumar

--- src/app/page.js
+++ src/app/page.js
@@ -1,134 +1,37 @@
 'use client'
 import 'regenerator-runtime/runtime'
-import Image from 'next/image'
-import styles from './page.module.css'
-import React, { useEffect, useState } from 'react';
-var mediaRecorder; // set mediaRecorder as  an globally accessible
-var audioText;
-const DG_KEY = "86ebe81f93d512a90cd655d04ca5e0985d341cf2";
-export default function Home() {
- let currentText = ''; // if you want record all audio even if you stoped and restart MediaRecorder, so you should set it as a global variable
+import React from 'react';
+import useSpeechToText from 'react-hook-speech-to-text';
 
-  const [record, setRecord] = useState(false);
-  const [text, setText] = useState('');
-  const [cc, setCc] = useState(false);
-  const [animation, setAnimation] = useState(false);
+export default function AnyComponent() {
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    crossBrowser: true,
+    googleApiKey: 'AIzaSyBYFQzw3V4bDy8nV1IT0euVcEtJ6MzUX4U',
+    useLegacyResults: false
+  });
 
-  const startRec = () => {
-    setRecord(true);
-    setText('');
-  };
-
-  const stopRec = async () => {
-    if (record && mediaRecorder.state !== 'inactive') {
-      mediaRecorder.stop();
-      mediaRecorder.stream.stop();
-      mediaRecorder.stream.getTracks().filter((i) => i.stop());
-      mediaRecorder = null;
-    }
-    setRecord(false);
-  };
-
-  const deepGramAudio2text = () => {
-    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-      if (!MediaRecorder.isTypeSupported('audio/webm')) {
-        return alert('Browser not supported');
-      }
-
-      var options = { mimeType: 'video/webm' };
-      mediaRecorder = new MediaRecorder(stream, options);
-
-      const socket = new WebSocket(`wss://api.deepgram.com/v1/listen`, [
-        'token',
-        DG_KEY,
-      ]);
-
-      socket.onopen = () => {
-        mediaRecorder.addEventListener('dataavailable', async (event) => {
-          if (event.data.size > 0 && socket.readyState == 1) {
-            socket.send(event.data);
-          }
-        });
-      };
-
-      mediaRecorder.start(1100);
-      console.log('started');
-
-      socket.onmessage = async (message) => {
-        const received = JSON.parse(message.data);
-        const transcript = received.channel.alternatives[0].transcript;
-        if (transcript && received.is_final) {
-          currentText = currentText.concat(' ' + transcript);
-          audioText = currentText;
-          console.log(audioText);
-          setText(audioText);
-        }
-      };
-    });
-  };
-
-  useEffect(() => {
-    if (record) {
-      deepGramAudio2text();
-    }
-  }, [record]);
-
-  let leters = text.split(' ');
-  let last2 = leters.slice(-2);
+  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
 
   return (
     <div>
-      <div className='homePage'>
-        <a href='https://deepgram.com' target='_blank'>
-          Deepgram AI 🤖
-        </a>
-
-        <div style={{ display: animation ? 'block' : 'none' }}>
-          <p className={`rotatingText-adjective hide`}>
-            {leters.slice(-14, -7).map((i) => i + ' ')}
-          </p>
-          <p className={`rotatingText-adjective show`}>
-            {leters.slice(-7).map((i) => i + ' ')}
-          </p>
-        </div>
-
-        <div>
-          <p>{leters.slice(-14, -7).map((i) => i + ' ')}</p>
-          <div style={{ display: 'flex', marginTop: '5px' }}>
-            <p>{leters.slice(-7, -2).map((i) => i + ' ')}</p>
-        
-          </div>
-        </div>
-
-        <button onClick={startRec} type='button' id='start'>
-          Start
-        </button>
-        <button onClick={stopRec} type='button' id='stop'>
-          Stop
-        </button>
-
-        <p>
-          click <a href='#start'>start</a> button speak, wait, finaly click{' '}
-          <a href='#stop'>stop</a> button
-        </p>
-        <p>
-          <a onClick={() => setCc((state) => !state)}>
-            {cc ? 'hide cc' : 'show cc'}
-          </a>
-        </p>
-        <p>
-          <a onClick={() => setAnimation((state) => !state)}>
-            {cc ? 'hide animation' : 'show animation'}
-          </a>
-        </p>
-      </div>
-      <textarea
-        className='textarea'
-        type='text'
-        value={text}
-        readOnly
-        style={{ display: cc ? 'block' : 'none' }}
-      />
+      <h1>Recording: {isRecording.toString()}</h1>
+      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+        {isRecording ? 'Stop Recording' : 'Start Recording'}
+      </button>
+      <ul>
+        {results.map((result) => (
+          <li key={result.timestamp}>{result.transcript}</li>
+        ))}
+        {interimResult && <li>{interimResult}</li>}
+      </ul>
     </div>
   );
 }
